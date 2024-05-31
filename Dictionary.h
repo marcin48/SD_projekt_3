81a6023a61704fa504bbabbe38843724debff80e
nowak-tymon <164738134+nowak-tymon@users.noreply.github.com>
2024-05-31 17:07:03 +0200

    Add files via upload

--- Dictionary.h
+++ Dictionary.h
@@ -3,9 +3,9 @@ class Dictionary
 {
 public:
     virtual void insert(int key, int value)=0;
-    virtual int search(int key)=0;
     virtual void remove(int key)=0;
     virtual int getRandomKey() = 0;
     virtual void display()=0;
+    virtual int getSize() const=0;
 };
 
